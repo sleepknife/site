01547c47cd73b83dd9b0a6040d7a1c2101d52f36
sleepknife <jacobdan992@gmail.com>
2025-05-15 15:10:28 -0500

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import LandingScreen from './components/LandingScreen';
+
+function App() {
+  return (
+    <div className="min-h-screen w-full">
+      <LandingScreen />
+    </div>
+  );
+}
+
+export default App;
